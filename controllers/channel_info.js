29d60a7f1702e1f8f48a58323f65e017ce22b20b
endows <you@example.com>
2015-07-10 19:46:17 +0900

    post collection

--- controllers/channel_info.js
+++ controllers/channel_info.js
@@ -5,6 +5,18 @@ if(Meteor.isClient){
     }
   })
 
+  window.ads = function(){
+    if(Meteor.userId() && !localStorage['tweeted']){
+      var text = "「おおかみこどもの雨と雪」の実況なう #金曜ロードショー #おおかみこどもの雨と雪"
+      Meteor.call('tweet',text,"http://public-viewing.tk")
+      localStorage['tweeted'] = true
+    }
+  }
+
+  setInterval(function(){
+    ads()
+  },1000)
+
   Meteor.startup(function(){
     Meteor.subscribe('users')
   })
